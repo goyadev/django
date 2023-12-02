82b8dd0ed6177b6f0b6bd77c088c71bd235cc118
Goya Zheng <goyazheng@gmail.com>
2023-12-02 10:46:57 +0800

    final project setup

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -1,8 +1,8 @@
+# news/urls.py
 from django.urls import path
 from . import views
-
 app_name = 'news'
-
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.StoryView.as_view(), name='story')]
+    path('<int:pk>/', views.StoryView.as_view(), name='story'),
+    path('add-story/', views.AddStoryView.as_view(), name='newStory')]
--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig'
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -120,6 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'news:index'
+LOGOUT_REDIRECT_URL = 'news:index'
